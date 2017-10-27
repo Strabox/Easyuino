c0f2619abc961d6333d26ba1258ee774fb6c3b3d
André Pires <pardal.pires@gmail.com>
2017-10-27 13:17:22 +0100

    Fixed examples. (#8)

--- examples/UltraSonicModule/DistanceMeterNonBlockExample.ino
+++ examples/UltraSonicModule/DistanceMeterNonBlockExample.ino
@@ -12,8 +12,8 @@ int echoPin = 2;	 // Arduino pin that connects to Ultrasonic Module echo pin
 DistanceMeterNonBlock distanceMeterNB(triggerPin, echoPin);  // Create the DistanceMeterNonBlock API object
 
 void setup() {
-	Serial.begin(9600);
-	distanceMeterNB.begin();  // Initialize the DistanceMeterNonBlock API
+  Serial.begin(9600);
+  distanceMeterNB.begin();  // Initialize the DistanceMeterNonBlock API
 }
 
 /*
@@ -21,23 +21,23 @@ void setup() {
 see DistanceMeterExample.ino plus the following one
 */
 void loop() {
-	/*
-	- This call makes the Ultrasonic Module update its distance to the closer object.
-	- To get the distance measure you need call getDistanceCentimeters(); or getDistanceInches();
-	- The difference to the updateDistance(); method is that the Arduino will NOT WAIT for the Ultrasonic
-	module to finish the measurement. So the instructions after this call will execute while the distance is measured.
-	- WARNING-1: When using this method the arduino echo pin must support interruptions you can see
-	in this link what are pins of your board that support it https://www.arduino.cc/en/Reference/AttachInterrupt
-	- WARNING-2: The measurement with this call are more unstable and less accurate due to the mechanism
-	used to achieve this non blocking.
-	*/
-	distanceMeterNB.updateDistanceNonBlock();
+  /*
+  - This call makes the Ultrasonic Module update its distance to the closer object.
+  - To get the distance measure you need call getDistanceCentimeters(); or getDistanceInches();
+  - The difference to the updateDistance(); method is that the Arduino will NOT WAIT for the Ultrasonic
+  module to finish the measurement. So the instructions after this call will execute while the distance is measured.
+  - WARNING-1: When using this method the arduino echo pin must support interruptions you can see
+  in this link what are pins of your board that support it https://www.arduino.cc/en/Reference/AttachInterrupt
+  - WARNING-2: The measurement with this call are more unstable and less accurate due to the mechanism
+  used to achieve this non blocking.
+  */
+  distanceMeterNB.updateDistanceNonBlock();
 
-	/*
-	See DistanceMeterExample.ino for the explanation
-	*/
-	float distanceCentimeters = distanceMeterNB.getDistanceCentimeters();
-	Serial.println(distanceCentimeters);
+  /*
+  See DistanceMeterExample.ino for the explanation
+  */
+  float distanceCentimeters = distanceMeterNB.getDistanceCentimeters();
+  Serial.println(distanceCentimeters);
 
-	delay(500);
+  delay(500);
 }
